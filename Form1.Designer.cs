155c7d06c34123126a0f6167b1fcc09027813212
jfordham8 <mrjkf3991@gmail.com>
2016-05-02 15:09:14 -0400

    Added files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Slappy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label1.Location = new System.Drawing.Point(282, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "time";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label2.Location = new System.Drawing.Point(1, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 42);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Current Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label3.Location = new System.Drawing.Point(31, 106);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(216, 42);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Alarm Time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label4.Location = new System.Drawing.Point(282, 106);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 42);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "alarm";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Chartreuse;
+            this.button2.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button2.Location = new System.Drawing.Point(342, 164);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 45);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "D";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Chartreuse;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Location = new System.Drawing.Point(289, 164);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "U";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label5.Location = new System.Drawing.Point(144, 159);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 42);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Hour";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label6.Location = new System.Drawing.Point(111, 202);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(136, 42);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Minute";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Chartreuse;
+            this.button3.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button3.Location = new System.Drawing.Point(289, 209);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 45);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "U";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Chartreuse;
+            this.button4.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button4.Location = new System.Drawing.Point(342, 209);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(45, 45);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM7";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(289, 318);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 31);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "http://";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(504, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(948, 501);
+            this.webBrowser1.TabIndex = 7;
+            this.webBrowser1.Url = new System.Uri("http://www.google.com", System.UriKind.Absolute);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold);
+            this.label7.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label7.Location = new System.Drawing.Point(53, 307);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(181, 42);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Server IP";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(289, 427);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(172, 40);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Set Alarm";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(8, 490);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 10;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(60, 369);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(174, 40);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Create Alarm";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(289, 369);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(172, 40);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Save Alarm";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(172, 471);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(108, 50);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Reset";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1464, 525);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slappy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.IO.Ports.SerialPort serialPort2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button6;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Net;
+using System.IO;
+
+namespace Slappy
+{
+    public partial class Form1 : Form
+    {
+        DateTime alarm = DateTime.Now;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            //COM7.Open();
+            button1.Text = char.ConvertFromUtf32(8593);
+            button2.Text = char.ConvertFromUtf32(8595);
+            button3.Text = char.ConvertFromUtf32(8593);
+            button4.Text = char.ConvertFromUtf32(8595);
+            label4.Text = alarm.ToString("hh:mm tt");
+            string time = DateTime.Now.ToString("hh:mm tt");
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("hh:mm tt");
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //label4.Text = DateTime.Now.ToString("HH:mm");
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            alarm = alarm.AddHours(1);
+            label4.Text = alarm.ToString("hh:mm tt");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            alarm = alarm.AddHours(-1);
+            label4.Text = alarm.ToString("hh:mm tt");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            alarm = alarm.AddMinutes(1);
+            label4.Text = alarm.ToString("hh:mm tt");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            alarm = alarm.AddMinutes(-1);
+            label4.Text = alarm.ToString("hh:mm tt");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //string requestmethod = "POST";
+            //string postData = "var1=Hello&var2=Server!";
+            //byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            //string URL = "http://www.google.com";
+            string URL = textBox1.Text;
+            int hour = (alarm.Hour % 12);
+            string hour1 = hour.ToString();
+            int min = alarm.Minute;
+            string min1 = min.ToString();
+            //string arg1 = alarm.Hour.ToString();
+            string arg2 = alarm.Minute.ToString();
+            string ampm = alarm.ToString("tt");
+            if (ampm == "PM")
+            {
+                ampm = "1";
+            }
+            else if (ampm == "AM")
+            {
+                ampm = "0";
+            }
+            //Uri uriAddress = new Uri(textBox1.Text + '/' + hour + '/' + min + '/');
+            string url1 = textBox1.Text;
+            string test = "/Alarm/updateAlarmPreference?arg1=";
+            string test2 = "&arg2=";
+            Uri uriAddress = new Uri(url1 + test + hour1 + test2 + min1 + test2 + ampm);
+            webBrowser1.Url = uriAddress;
+            textBox2.Text = uriAddress.ToString();
+            //string contenttype = "application/x-www-form-urlencoded";
+            //string responseFromServer = null;
+            //WebRequest request = WebRequest.Create(URL);
+            //Stream dataStream;            
+            //WebResponse response;            
+            //StreamReader reader;            
+            //request.Method = requestmethod;
+            //request.ContentType = contenttype;
+            //request.ContentLength = byteArray.Length;
+            //dataStream = request.GetRequestStream();
+            //dataStream.Write(byteArray, 0, byteArray.Length);
+            //dataStream.Close();           
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            string URL = textBox1.Text;
+            Uri uriAddress = new Uri(textBox1.Text + "/setAlarm");
+            webBrowser1.Url = uriAddress;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string URL = textBox1.Text;
+            Uri uriAddress = new Uri(textBox1.Text + "/Alarm/new?name=Alarm");
+            webBrowser1.Url = uriAddress;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string URL = textBox1.Text;
+            int hour = (alarm.Hour % 12);
+            string hour1 = hour.ToString();
+            int min = alarm.Minute;
+            string min1 = min.ToString();
+            //string arg1 = alarm.Hour.ToString();
+            string arg2 = alarm.Minute.ToString();
+            string ampm = alarm.ToString("tt");
+            if (ampm == "PM")
+            {
+                ampm = "1";
+            }
+            else if (ampm == "AM")
+            {
+                ampm = "0";
+            }
+            //Uri uriAddress = new Uri(textBox1.Text + '/' + hour + '/' + min + '/');
+            string url1 = textBox1.Text;
+            string test = "/Alarm/updateAlarmPreference?arg1=";
+            string test2 = "&arg2=";
+            Uri uriAddress = new Uri(url1 + test + hour1 + test2 + min1 + test2 + ampm);
+            webBrowser1.Url = uriAddress;
+            textBox2.Text = uriAddress.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string url1 = "http://www.google.com";
+            Uri uriAddress = new Uri(url1);
+            webBrowser1.Url = uriAddress;
+        }
+    }
+}
